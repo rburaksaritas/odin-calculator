fa61f7c8c96cf6e83aabb2f6c20e409a97d0f0f6
rburaksaritas <rburaksaritas@gmail.com>
2022-07-26 21:26:34 +0300

    Add feature press button and press equals
    Add functionality to the operation buttons in order to do operations.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+let operationPressed = "";
+let previousNumber = 0;
+let currentNumber = 0;
+
 let changeDisplay = function(e){
     const pressedNumber = this.textContent;
     const display = document.getElementById("display");
     const displayValue = display.textContent;
     if (displayValue=="0"||displayValue=="+"||displayValue=="-"
-            ||displayValue=="/"||displayValue=="x"){
+            ||displayValue=="/"||displayValue=="x" || displayValue==""){
         display.textContent = pressedNumber;       
     } else {
         if(displayValue.length<15){
@@ -40,9 +44,8 @@ deleteButton.addEventListener("click", deleteLastElement);
 let clear = function(e){
     const display = document.getElementById("display");
     display.textContent = "0";
-    /** TODO: delete stored numbers in memory 
-     * after functionality is implemented
-    */
+    previousNumber = 0;
+    currentNumber = 0;
 }
 
 const clearButton = document.getElementById("clear-button");
@@ -72,3 +75,45 @@ let operate = function(operator, number1, number2){
     if (operator == "x") {return multiply(number1,number2);}
     if (operator == "/") {return divide(number1,number2);}
 }
+
+let pressOperation = function(e){
+    /* operation button pressed for the first time */
+    const display = document.getElementById("display");
+    if(!operationPressed){
+        previousNumber = parseFloat(display.textContent);
+        display.textContent = this.textContent;
+        operationPressed = this.textContent;
+    } 
+    /* operation button pressed later */
+    else{
+        currentNumber = parseFloat(display.textContent);
+        if (isNaN(currentNumber)){
+            currentNumber = parseFloat(previousNumber);  
+        }
+        previousNumber = operate(operationPressed, previousNumber, currentNumber);
+        currentNumber = 0;
+        display.textContent = this.textContent;
+        operationPressed = this.textContent;
+    }
+}
+
+const operationButtons = document.querySelectorAll(".operation-button");
+operationButtons.forEach(button => button.addEventListener("click", pressOperation));
+
+let pressEqual = function(e){
+    const display = document.getElementById("display");
+    currentNumber = parseFloat(display.textContent);
+    if (isNaN(currentNumber)){
+        currentNumber = 0;  
+    }
+    let result = operate(operationPressed, previousNumber, currentNumber);
+    currentNumber = result;
+    if (currentNumber.length>15){
+        currentNumber = parseFloat(toString(currentNumber).slice(0,15));
+    } 
+    display.textContent = currentNumber;
+    operationPressed = "";
+}
+
+const equalsButton = document.getElementById("button-equals");
+equalsButton.addEventListener("click", pressEqual);
